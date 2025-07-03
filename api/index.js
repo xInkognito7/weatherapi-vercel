67ab3596c09c8eeb83c068c7e9ced3234f826f79
xInkognito7 <sebastiangrau1216@gmail.com>
2025-07-03 04:36:29 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const apiKey = process.env.WEATHER_API_KEY;
 
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=de`);
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=de`);
     if (!response.ok) {
       throw new Error(`Weather API error: ${response.status}`);
     }
